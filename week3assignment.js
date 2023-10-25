92101041759b75def4c733d4c5548b1bb142b3bd
musango willy <musangowilly@gmail.com>
2023-10-25 13:03:05 +0300

    week 3 assignment

---
+++ week3assignment.js
@@ -0,0 +1,105 @@
+/question 1
+
+const number = "45";
+
+console.log(number);
+
+//question 2
+
+//const printSum = "20" + 40. //let the result print 60 //hint: 60.
+
+let x=20;
+x+=40;
+console.log(x);
+
+
+//const sentence = "I am " + 23 + 4 + " " +"years old" //hint: “I am 27 years old”
+
+let y="I am"+23+4 + " " +"years old";
+
+
+console.log(y);
+
+
+//question 3
+
+
+const myValue = 86;
+
+console.log(myValue);
+
+
+//question 4
+
+
+let FirstName="musango";
+
+console.log(FirstName);
+
+let LastName="willy";
+
+console.log(LastName);
+
+let GuruCodingCourse="JavaScript";
+
+console.log(GuruCodingCourse);
+
+let TestScore="100%";
+
+console.log(TestScore);
+
+
+//question 5
+
+
+const mayJavaScriptTestScore="86";
+
+
+if(mayJavaScriptTestScore<30 ){
+
+    console.log("May failed the test,May is a F9 student");
+
+}else if(mayJavaScriptTestScore>=31 && mayJavaScriptTestScore<=40){
+
+    console.log("May is a E student");
+
+}else if(mayJavaScriptTestScore>=41 && mayJavaScriptTestScore<=49){
+
+    console.log("May is a D student");
+
+}else if(mayJavaScriptTestScore>=50 && mayJavaScriptTestScore<=59){
+
+    console.log("May is a C student");
+
+}else if(mayJavaScriptTestScore<=60 && mayJavaScriptTestScore<=69){
+
+    console.log("May is a B student");
+
+}else if(mayJavaScriptTestScore>=70){
+
+    console.log("May is a A student");
+
+}else if(mayJavaScriptTestScore>=101){
+
+    console.log("testScore is out of range");
+
+}else {//anything that is negative
+
+    console.log("testScore is no score");
+
+}
+
+
+//question 6
+
+for(let count=1;count<=20;count++){
+
+    console.log(count);
+}
+
+
+ //for loop iterates to check spcefied conditions and then repeatedly executes a block of code as long as those conditions are met.
+ //declared a variable named "count" and initilized it 
+ // count<=20 it is the conditions that the for loop must evaluate and met
+ //count++ it increments the value of count +1 after each iteration until it meets the conditions
+ //console.log(count) prints the output of the loop after executing the block of code
